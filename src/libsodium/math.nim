ba36449396fe8000a056ea69e74441b7e4eb54c8
Geoffrey Picron <geoffrey.picron@arboratum.com>
2023-04-28 15:52:25 +0200

    add ed to curve conversion

--- src/libsodium/math.nim
+++ src/libsodium/math.nim
@@ -4,6 +4,7 @@
 import libsodium_abi
 import publicbox
 import secretbox
+import signature
 import private/utils
 import common
 
@@ -12,3 +13,15 @@ func scalarMult*(n: BoxPrivateKey, p: BoxPublicKey) : SecretBoxKey {.inline.} =
     ## https://download.libsodium.org/doc/advanced/scalar_multiplication
     
     check_rc crypto_scalarmult(result.address, n.address, p.address)
+
+func signKeyToBoxKey*(pk: SignPublicKey) : BoxPublicKey {.inline.} =
+    ## Converts a sign public key to a box public key
+    ## https://download.libsodium.org/doc/advanced/ed25519-curve25519
+    
+    check_rc crypto_sign_ed25519_pk_to_curve25519(result.address, pk.address)
+
+func signKeyToBoxKey*(sk: SignPrivateKey) : BoxPrivateKey {.inline.} =
+    ## Converts a sign secret key to a box secret key
+    ## https://download.libsodium.org/doc/advanced/ed25519-curve25519
+    
+    check_rc crypto_sign_ed25519_sk_to_curve25519(result.address, sk.address)
